1c982ea8b78955ace85ec666c809f0e34a722c19
igor.guariroba <igor.guariroba@jsl.com.br>
2021-10-31 22:16:13 -0300

    Filter var imput

--- www/mvc/public/index.php
+++ www/mvc/public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
 
 switch ($_SERVER['REQUEST_URI']):
     case "/api/listar-cursos":
@@ -15,6 +16,10 @@ switch ($_SERVER['REQUEST_URI']):
     case "/api/novo-curso":
     case "/api/novo-curso/":
         $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+    case "/api/salva-curso/":
+        $controlador = new Persistencia();
         $controlador->processaRequisicao();
         break;
     default :
---
+++ www/mvc/src/Controller/Persistencia.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    private EntityManagerCreator $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager();
+    }
+
+    public function processarequisicao(): void
+    {
+        $decricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
+
+        $curso = new Curso();
+        $curso->setDescricao($decricao);
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+    }
+}
