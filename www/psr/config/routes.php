c78f97c3eb843d29f7695fbe57baec332252f339
igor.guariroba <igor.guariroba@jsl.com.br>
2021-11-06 17:45:02 -0300

    Add rota para salvar e alterar curso

--- www/psr/config/routes.php
+++ www/psr/config/routes.php
@@ -3,8 +3,10 @@
 
 use Alura\Psr\Controller\FormularioInsercao;
 use Alura\Psr\Controller\ListarCursos;
+use Alura\Psr\Controller\Persistencia;
 
 return [
     '/api-psr/novo-curso' => FormularioInsercao::class,
-    '/api-psr/listar-cursos' => ListarCursos::class
+    '/api-psr/listar-cursos' => ListarCursos::class,
+    '/api-psr/salvar-curso' => Persistencia::class
 ];
